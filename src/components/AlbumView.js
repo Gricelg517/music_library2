6fa7cde4316f0edd153836751acdd0fe6baf295b
Gricelg517 <135929737+Gricelg517@users.noreply.github.com>
2023-10-11 17:20:53 -0500

    create project

---
+++ src/components/AlbumView.js
@@ -0,0 +1,38 @@
+// These components will be making separate API calls from the app
+// component to serve specific data about a given album
+import { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+
+function AlbumView() {
+    const { id } = useParams()
+    const [ albumData, setAlbumData ] = useState([])
+
+    useEffect(() => {
+        const API_URL = `http://localhost:4000/song/${id}`
+
+        const fetchData = async () => {
+            const response = await fetch(API_URL)
+            const data = await response.json()
+            console.log(data)
+            const albumData = data.results.filter(item => item.wrapperType === 'track')
+            setAlbumData(albumData)
+        }
+        fetchData()
+    }, [id])
+    
+    const albumDataDisplay = albumData.map(track => {
+        return (
+            <div key={track.trackId}>
+                <p>{track.trackName}</p>
+            </div>
+        )
+    })
+
+    return (
+        <div>
+            {albumDataDisplay}
+        </div>
+    )
+}
+
+export default AlbumView
